847013cc5f49179b4d3eec8a630b78f10e37cdc4
Ozzy Dagci <35441651+oguzhandagci@users.noreply.github.com>
2022-05-24 01:02:19 -0400

    First Commit

---
+++ src/Animals/NoiseCapable.java
@@ -0,0 +1,4 @@
+package Animals;
+public interface NoiseCapable {
+    public void makeNoise();
+}
---
+++ src/Herds/Herd.java
@@ -0,0 +1,31 @@
+package Herds;
+import java.util.ArrayList;
+
+public class Herd implements Movable {
+
+    private ArrayList<Movable> herd;
+
+    public Herd() {
+        this.herd = new ArrayList<>();
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        for(Movable animal: herd){
+            animal.move(dx, dy);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String print = "";
+        for(Movable animal : herd){
+            print += animal.toString() + "\n";
+        }
+        return print;
+    }
+
+    public void addToHerd(Movable movable){
+        herd.add(movable);
+    }
+}
